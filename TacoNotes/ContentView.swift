ceab8898a0276889bd99ea5da7cb55d51be0e54f
Oliver Johnson <oa_johnson@outlook.com>
2022-09-06 01:28:37 -0500

    Initialize project

---
+++ TacoNotes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TacoNotes
+//
+//  Created by Oliver Johnson on 9/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TacoNotes/TacoNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TacoNotesApp.swift
+//  TacoNotes
+//
+//  Created by Oliver Johnson on 9/6/22.
+//
+
+import SwiftUI
+
+@main
+struct TacoNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
